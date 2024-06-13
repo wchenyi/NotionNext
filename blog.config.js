077f9200d70a5d9703859b82b2a04edebaac242e
Wangcy <93115719+wchenyi@users.noreply.github.com>
2024-06-13 18:28:22 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,13 +20,13 @@ const BLOG = {
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi，我是一个瞌睡虫🥱, Hi，我是一个法学🐶, Hi，我是一个干饭人,欢迎来到我的博客🎉',
+    'Hi，我是菜头🥕, Hi，我是一个狗子🐶,欢迎来到相册🎉',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Wangcy', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'caitou', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍜', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://wangcy.cam', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || '', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
