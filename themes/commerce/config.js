4934950d243117796f615365d871d01a3daebdf8
Wangcy <93115719+wchenyi@users.noreply.github.com>
2024-07-10 00:16:42 +0800

    Update config.js

--- themes/commerce/config.js
+++ themes/commerce/config.js
@@ -5,10 +5,10 @@ const CONFIG = {
   COMMERCE_TEXT_CENTER_TITLE: '内容列表', // 中间产品区块标题
   COMMERCE_TEXT_CENTER_DESCRIPTION:
     '', // 中间产品区块文字描述
-  COMMERCE_TEXT_CENTER_CATEGORY_TITLE: 'Product Categories', // 左侧产品分类标题
-  COMMERCE_TEXT_FOOTER_TITLE: 'Contact US', // COMMERCE主题页脚文案标题；按Shift+Enter键可以换行
-  COMMERCE_TEXT_FOOTER_MENU_1: 'Product Center', // COMMERCE主题页脚左侧菜单标题1
-  COMMERCE_TEXT_FOOTER_MENU_2: 'About US', // COMMERCE主题页脚左侧菜单标题2
+  COMMERCE_TEXT_CENTER_CATEGORY_TITLE: '内容分类', // 左侧产品分类标题
+  COMMERCE_TEXT_FOOTER_TITLE: '联系作者', // COMMERCE主题页脚文案标题；按Shift+Enter键可以换行
+  COMMERCE_TEXT_FOOTER_MENU_1: '内容分类', // COMMERCE主题页脚左侧菜单标题1
+  COMMERCE_TEXT_FOOTER_MENU_2: '关于页面', // COMMERCE主题页脚左侧菜单标题2
 
   COMMERCE_FOOTER_RIGHT_IMG_URL: null, // 显示页脚右侧的图片，通常放二维码
   COMMERCE_FOOTER_RIGHT_TEXT: null, // 页脚右侧图片下的文字描述
