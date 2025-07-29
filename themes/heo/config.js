d29de6eba32b9ae182e948bd792478a578b97057
Wangcy <93115719+wchenyi@users.noreply.github.com>
2025-07-29 10:40:22 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,11 +18,11 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
+  HEO_HERO_TITLE_1: '分享生活',
+  HEO_HERO_TITLE_2: '与思维探索',
+  HEO_HERO_TITLE_3: 'WANGCY.MY',
+  HEO_HERO_TITLE_4: '点开阅读',
+  HEO_HERO_TITLE_5: '关于本站的相关内容',
   HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
